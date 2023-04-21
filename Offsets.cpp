4de341a3b1795410196c98ec313cc8bb001c67f3
LR-KING <92388638+LR-KING@users.noreply.github.com>
2023-04-21 20:06:30 +0800

    Update Offsets.cpp

--- Offsets.cpp
+++ Offsets.cpp
@@ -5,16 +5,16 @@ namespace offsets
 {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long IN_ATTACK = 0x07678368;            // [Buttons] -> in_attack
+    const long IN_ATTACK = 0x07871c50;            // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long REGION = 0x140000000;              // [Mine]          -> Region
-    const long LEVEL = 0x1462450;                 // [Miscellaneous] -> LevelName
-    const long LOCAL_PLAYER = (0x01ee8d58);       // [Miscellaneous] -> AVC_GameMovement+0x8
-    const long ENTITY_LIST = 0x1b37b18;           // [Miscellaneous] -> cl_entitylist
+    const long LEVEL = 0x1639960;                 // [Miscellaneous] -> LevelName
+    const long LOCAL_PLAYER = (0x020c1130);       // [Miscellaneous] -> AVC_GameMovement+0x8
+    const long ENTITY_LIST = 0x1d0fe58;           // [Miscellaneous] -> cl_entitylist
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const long LOCAL_ORIGIN = 0x0158; // [DataMap.CBaseViewModel]    -> m_localOrigin
+    const long LOCAL_ORIGIN = 0x0004; // [DataMap.CBaseViewModel]    -> m_localOrigin
     // player
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     const long GLOW_ENABLE = (0x03c0 + 0x8);        // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
@@ -27,6 +27,6 @@ namespace offsets
     const long VIEW_ANGLE = (0x25ac - 0x14);        // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
     const long BLEEDOUT_STATE = 0x2740;             // [RecvTable.DT_Player]            -> m_bleedoutState
     const long ZOOMING = 0x1c51;                    // [RecvTable.DT_Player]            -> m_bZooming
-    const long LAST_VISIBLE_TIME = 0x1A78;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
+    const long LAST_VISIBLE_TIME = 0x1a78;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
     const long CURRENT_SHIELDS = 0x0170;             // m_shieldHealth
 }
